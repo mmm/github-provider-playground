9063f2c830838ae6dff733e321f511c1a20f2029
Mark Mims <markmims@google.com>
2024-03-14 06:55:45 -0600

    first pass at a test

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Playground to test out some GitHub repo automation
 
+The provider seems well-maintained. I'm curious about how well this is working
+nowadays... and decisions the team have made about how to deal with the various
+conflict scenarios when trying to use Terraform to manage resources that live
+in git repos.  Let's break things!
 
+Gonna start just testing terraform that manages resources within this same
+repo. Mostly just to see right away how well the provider handles
+tightly-managed `github_repository_file` resources in a situation where the
+surrounding repo floats around.  Wonder what it'll do with caching local
+clones during an apply...
---
+++ terraform/configfiles/main.tf
@@ -0,0 +1,33 @@
+#
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+resource "github_repository" "playground" {
+  name      = "github-provider-playground"
+  auto_init = false
+}
+
+resource "github_repository_file" "foo" {
+  repository          = github_repository.playground.name
+  branch              = "main"
+  file                = "targets/foo"
+  content             = <<-EOT
+    some sort of content goes here
+    with more content here
+    EOT
+  commit_message      = "Managed by Terraform"
+  commit_author       = var.git_user_name
+  commit_email        = var.git_user_email
+  overwrite_on_create = true
+}
